0468c8ecb20655ac8943b3041393cdeef08d3cb5
stevengx <3428339304@qq.com>
2022-11-20 06:12:21 +0800

    test1

--- rl-app1/src/main/java/com/sg/rl/RLApplication1.java
+++ rl-app1/src/main/java/com/sg/rl/RLApplication1.java
@@ -2,7 +2,7 @@ package com.sg.rl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 
 @SpringBootApplication
@@ -10,6 +10,7 @@ public class RLApplication1 {
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
+        System.out.println("123");
         SpringApplication.run(RLApplication1.class, args);
 
     }
